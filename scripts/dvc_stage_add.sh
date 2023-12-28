6ccde0a6c82db350ff15142ae64ccb305317a7b4
AnupJoseph <anup20joseph@gmail.com>
2023-12-28 04:10:39 +0000

    Added spanish files

--- scripts/dvc_stage_add.sh
+++ scripts/dvc_stage_add.sh
@@ -1,1 +1,8 @@
 dvc stage add -n prepare  -d src/dataset_utils.py -d data/raw/spanish.zip python src/dataset_utils.py data/raw/spanish.zip
+
+stages:
+  prepare:
+    cmd: python src/dataset_utils.py data/raw/spanish.zip
+    deps:
+    - data/raw/spanish.zip
+    - src/dataset_utils.py
